95752f57864aa404018b6eae6b61e6bffe41011c
Kartikey Sharma <kartikey.bluebells@gmail.com>
2022-08-07 21:55:19 +0530

    update files

--- app/src/main/java/com/example/notesapplication/AddEditNoteActivity.kt
+++ app/src/main/java/com/example/notesapplication/AddEditNoteActivity.kt
@@ -17,27 +17,24 @@ class AddEditNoteActivity : AppCompatActivity() {
     lateinit var noteEdt: EditText
     lateinit var saveBtn: Button
 
-    //on below line we are creating variable for viewmodal and and integer for our note id.
-    lateinit var viewModal: NoteViewModel
+    lateinit var viewModel: NoteViewModel
     var noteID = -1;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_edit_note)
-        //on below line we are initlaiing our view modal.
-        viewModal = ViewModelProvider(
+
+        viewModel = ViewModelProvider(
             this,
             ViewModelProvider.AndroidViewModelFactory.getInstance(application)
         ).get(NoteViewModel::class.java)
-        //on below line we are initializing all our variables.
+
         noteTitleEdt = findViewById(R.id.idEdtNoteName)
         noteEdt = findViewById(R.id.idEdtNoteDesc)
         saveBtn = findViewById(R.id.idBtn)
 
-        //on below line we are getting data passsed via an intent.
         val noteType = intent.getStringExtra("noteType")
         if (noteType.equals("Edit")) {
-            //on below line we are setting data to edit text.
             val noteTitle = intent.getStringExtra("noteTitle")
             val noteDescription = intent.getStringExtra("noteDescription")
             noteID = intent.getIntExtra("noteId", -1)
@@ -48,27 +45,23 @@ class AddEditNoteActivity : AppCompatActivity() {
             saveBtn.setText("Save Note")
         }
 
-        //on below line we are adding click listner to our save button.
         saveBtn.setOnClickListener {
-            //on below line we are getting title and desc from edit text.
             val noteTitle = noteTitleEdt.text.toString()
             val noteDescription = noteEdt.text.toString()
-            //on below line we are checking the type and then saving or updating the data.
             if (noteType.equals("Edit")) {
                 if (noteTitle.isNotEmpty() && noteDescription.isNotEmpty()) {
                     val sdf = SimpleDateFormat("dd MMM, yyyy - HH:mm")
                     val currentDateAndTime: String = sdf.format(Date())
                     val updatedNote = Note(noteTitle, noteDescription, currentDateAndTime)
                     updatedNote.id = noteID
-                    viewModal.updateNote(updatedNote)
+                    viewModel.updateNote(updatedNote)
                     Toast.makeText(this, "Note Updated..", Toast.LENGTH_LONG).show()
                 }
             } else {
                 if (noteTitle.isNotEmpty() && noteDescription.isNotEmpty()) {
                     val sdf = SimpleDateFormat("dd MMM, yyyy - HH:mm")
                     val currentDateAndTime: String = sdf.format(Date())
-                    //if the string is not empty we are calling a add note method to add data to our room database.
-                    viewModal.addNote(Note(noteTitle, noteDescription, currentDateAndTime))
+                    viewModel.addNote(Note(noteTitle, noteDescription, currentDateAndTime))
                     Toast.makeText(this, "$noteTitle Added", Toast.LENGTH_LONG).show()
                 }
             }
